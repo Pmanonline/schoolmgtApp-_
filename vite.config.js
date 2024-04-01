99920eb47b7fee736401df5e68b0a161071c0f14
pmanonline <petersonzoconis@gmail.com>
2024-04-01 12:12:27 +0100

    changed port

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Change the port to 3000
+    host: "0.0.0.0", // Bind to all network interfaces
+    port: 10000, // Change the port to 3000 or any other desired port
   },
   esbuild: {
     jsxFactory: "jsx",
